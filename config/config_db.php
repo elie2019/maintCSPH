c31e3ab1476307dd8785da9009187c779716f17a
Matcheme Elie <eliematcheme@gmail.com>
2019-12-01 15:56:36 +0100

    mise en place du glpi 9.4.4

---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost     = '127.0.0.1';
+   public $dbuser     = 'c0service';
+   public $dbpassword = 'Service%232019';
+   public $dbdefault  = 'c0service';
+}
